0ddab1c92dbeb3b72db66729087dc0af506532cb
LeahJKH <leahjb06.kodehode@gmail.com>
2024-05-31 11:19:00 +0200

    got images working

---
+++ site/js/images.js
@@ -0,0 +1,23 @@
+const cosplay = document.querySelector("#cosplay-pics")
+const transP = document.querySelector("#transparent-pics")
+const vocaloids = document.querySelector("#vocaloid-pics")
+const pfp = document.querySelector("#profile-pics")
+const fullbg = document.querySelector("#full-background-pics")
+
+
+displayImages(cosplay, 2, "cosplay", "c")
+displayImages(fullbg, 3, "fullBg", "fb")
+displayImages(pfp, 12, "pfp", "pfp")
+displayImages(transP, 5, "TransP", "tp")
+displayImages(vocaloids, 46, "vocaloids", "vo")
+
+
+function displayImages (container, number, folder, code) {
+
+    for (let i = 1; i <= number; i++) {
+        const img = document.createElement("img")
+        img.classList.add("img-sizing")
+        img.src = `../../images/${folder}/${code}${i}.webp` 
+        container.appendChild(img)
+    }
+}
