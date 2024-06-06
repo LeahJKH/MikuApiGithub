e621ef380675ef803fbb10799623e33899a77e89
LeahJKH <leahjb06.kodehode@gmail.com>
2024-06-06 13:17:04 +0200

    made sure logic works

--- site/js/images.js
+++ site/js/images.js
@@ -4,20 +4,19 @@ const vocaloids = document.querySelector("#vocaloid-pics")
 const pfp = document.querySelector("#profile-pics")
 const fullbg = document.querySelector("#full-background-pics")
 
-
 displayImages(cosplay, 2, "cosplay", "c")
-displayImages(fullbg, 17, "fullBg", "fb")
-displayImages(pfp, 12, "pfp", "pfp")
-displayImages(transP, 5, "TransP", "tp")
-displayImages(vocaloids, 49, "vocaloids", "vo")
+displayImages(fullbg,17, "fullBg", "fb");
+displayImages(pfp, 12, "pfp", "pfp");
+displayImages(transP, 5, "TransP", "tp");
+displayImages(vocaloids, 49, "vocaloids", "vo");
 
 function displayImages (container, number, folder, code) {
     
-        for (let i = 1; i <= number; i++) {
-            const img = document.createElement("img")
-            img.classList.add("img-sizing")
-            img.src = `../../images/${folder}/${code}${i}.webp` 
-            container.appendChild(img)
-        }
-    
+    for (let i = 1; i <= number; i++) {
+        const img = document.createElement("img")
+        img.classList.add("img-sizing")
+        img.src = `../../images/${folder}/${code}${i}.webp` 
+        container.appendChild(img)
+    }
+
 }
