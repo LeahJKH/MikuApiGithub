377b7ad37e582fd8d09b991af7271536d59337a0
LeahJKH <leahjb06.kodehode@gmail.com>
2024-06-13 13:15:18 +0200

    made ts files (need too be tested)

---
+++ ts/mikuVersions.ts
@@ -0,0 +1,317 @@
+[
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V2 standard",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo10.webp"
+    },
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V2 append",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo47.webp"
+    },
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V3 english",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo11.webp"
+    },
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V3 standard",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo48.webp"
+    },
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V4X standard",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo12.webp"
+    },
+    {
+        "name": "Hatsune Miku",
+        "codename": "CV01",
+        "introduction": "VOCALOID2",
+        "version": "V4 chinese",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo49.webp"
+    },
+    {
+        "name": "Act 1 Kagamine Rin",
+        "codename": "CV02",
+        "introduction": "VOCALOID2",
+        "version": "Original",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo15.webp"
+    },
+    {
+        "name": "Act 1 Kagamine Len",
+        "codename": "CV02",
+        "introduction": "VOCALOID2",
+        "version": "Original",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo18.webp"
+    },
+    {
+        "name": "Megurine Luka",
+        "codename": "CV03",
+        "introduction": "VOCALOID2",
+        "version": "V2 default",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo45.webp"
+    },
+    {
+        "name": "Megurine Luka",
+        "codename": "CV03",
+        "introduction": "VOCALOID2",
+        "version": "V4X standard",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo46.webp"
+    },
+    {
+        "name": "VY1",
+        "codename": "MIZKI",
+        "introduction": "VOCALOID2",
+        "version": "prank version",
+        "image": ""
+    },
+    {
+        "name": "VY2",
+        "codename": "Yuuma",
+        "introduction": "VOCALOID2",
+        "version": "professional version",
+        "image": ""
+    },
+    {
+        "name": "V1 Kaito",
+        "codename": "TARO",
+        "introduction": "VOCALOID",
+        "version": "Original",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo1.webp"
+    },
+    {
+        "name": "V2 Gumi/Megpoid",
+        "codename": "VA-M01",
+        "introduction": "VOCALOID2",
+        "version": "V2",
+        "image": ""
+    },
+    {
+        "name": "V1 Meiko",
+        "codename": "HANAKO",
+        "introduction": "VOCALOID",
+        "version": "V1",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo3.webp"
+    },
+    {
+        "name": "Miriam",
+        "codename": "none",
+        "introduction": "VOCALOID",
+        "version": "V1",
+        "image": "https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo5.webp"
+    },
+    {
+        "name": "Leon",
+        "codename":"none",
+        "introduction":"VOCALOID",
+        "version":"V1",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo6.webp"
+    },
+    {
+        "name": "Lola",
+        "codename":"none",
+        "introduction":"VOCALOID",
+        "version":"V1",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo7.webp"
+    },
+    {
+        "name": "standard Sweet ANN",
+        "codename":"JODIE",
+        "introduction":"VOCALOID2",
+        "version":"V2 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo8.webp"
+    },
+    {
+        "name": "Taiwanese Sweet ANN",
+        "codename":"JODIE",
+        "introduction":"VOCALOID2",
+        "version":"V2 Taiwanese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo9.webp"
+    },
+    {
+        "name": "standard Prima",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo13.webp"
+    },
+    {
+        "name": "Taiwanese Prima",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 Taiwanese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo14.webp"
+    },
+    {
+        "name": "Camui Gackpo",
+        "codename":"VA-G01",
+        "introduction":"VOCALOID2",
+        "version":"V2",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo21.webp"
+    },
+    {
+        "name": "standard SONiKA",
+        "codename":"ZGV5",
+        "introduction":"VOCALOID2",
+        "version":"V2 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo22.webp"
+    },
+    {
+        "name": "Debute SONiKA",
+        "codename":"ZGV5",
+        "introduction":"VOCALOID2",
+        "version":"V2 Debute",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo23.webp"
+    },
+    {
+        "name": "Chinese SONiKA",
+        "codename":"ZGV5",
+        "introduction":"VOCALOID2",
+        "version":"V2 Chinese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo24.webp"
+    },
+    {
+        "name": "Taiwanese SONiKA",
+        "codename":"ZGV5",
+        "introduction":"VOCALOID2",
+        "version":"V2 Taiwanese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo25.webp"
+    },
+    {
+        "name": "SF-A2 codename miki",
+        "codename":"miki",
+        "introduction":"VOCALOID2",
+        "version":"V2",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo27.webp" 
+    },
+    {
+        "name": "kaai yuki",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo28.webp" 
+    },
+    {
+        "name": "default Hiyama Kiyoteru",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 default",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo29.webp" 
+    },
+    {
+        "name": "ICE MOUNTAIN Hiyama Kiyoteru",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 ICE MOUNTAIN",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo30.webp" 
+    },
+    {
+        "name": "BIG AL",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 default",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo31.webp" 
+    },
+    {
+        "name": "Taiwanese BIG AL",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 Taiwanese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo32.webp" 
+    },
+    {
+        "name": "Debute BIG AL",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 Debute",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo33.webp" 
+    },
+    {
+        "name": "default Tonio",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 default",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo34.webp" 
+    },
+    {
+        "name": "Taiwanese Tonio",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 Taiwanese",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo35.webp" 
+    },
+    {
+        "name": "lily",
+        "codename":"VA-L01",
+        "introduction":"VOCALOID2",
+        "version":"V2 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo36.webp" 
+    },
+    {
+        "name": "lily",
+        "codename":"VA-L01",
+        "introduction":"VOCALOID2",
+        "version":"V3 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo37.webp" 
+    },
+    {
+        "name": "Ryuto",
+        "codename":"VA-GP01",
+        "introduction":"VOCALOID2",
+        "version":"V2 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo38.webp" 
+    },
+    {
+        "name": "Ryuto",
+        "codename":"VA-GP01",
+        "introduction":"VOCALOID2",
+        "version":"V3 standard",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo39.webp" 
+    },
+    {
+        "name": "Nekomura Iroha",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 default",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo40.webp" 
+    },
+    {
+        "name": "Nekomura Iroha",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V2 Kittyler",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo41.webp" 
+    },
+    {
+        "name": "Nekomura Iroha",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V4 Natural",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo42.webp" 
+    },
+    {
+        "name": "Nekomura Iroha",
+        "codename":"none",
+        "introduction":"VOCALOID2",
+        "version":"V4 Soft",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo43.webp" 
+    },
+    {
+        "name": "Utatane Piko",
+        "codename":"KSRW-1",
+        "introduction":"VOCALOID2",
+        "version":"V2 default",
+        "image":"https://leahjkh.github.io/MikuApiGithub/images/vocaloids/vo44.webp" 
+    }
+]
